cde5c456c04038574a37fdc254559ef4e0e1e8b0
Rodrigo Goulart Ribeiro <rribeiro@ciandt.com>
2021-02-01 19:31:30 -0300

    Cassia's solution

--- src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -6,11 +6,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-tac-toe.component.css']
 })
 export class TicTacToeComponent implements OnInit {
+  // private cleanPuzzle: string[] = ['', '', '', '', '', '', '', '', ''];
 
+  public player: boolean;
   public status: string;
+  public puzzle = ['', '', '', '', '', '', '', '', ''];
+
+  // public tictactoeData = 
 
   public ngOnInit() {
-    this.status = "Next player: X"
+    this.reset();
+    this.status = "Next player: ";
+  }
+
+  onClick(item) {
+
+    if (this.puzzle[item] !== '') {
+      return;
+    }
+    this.puzzle[item] = this.player ? 'x' : 'o';
+
+    console.log('current puzzle', this.puzzle);
+
+    const currentPlayerItens = this.puzzle.filter(square => {
+      return square === this.puzzle[item];
+    });
+
+    console.log('itensInside', currentPlayerItens);
+    if (currentPlayerItens.length >= 3) {
+      this.checkGameStatus();
+    }
+
+    this.player = !this.player;
+  }
+
+  public reset() {
+    this.player = true;
+    this.puzzle = ['', '', '', '', '', '', '', '', ''];
+  }
+
+  checkGameStatus() {
+
   }
 
 }
